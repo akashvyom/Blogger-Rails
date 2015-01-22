7059eb964e1026dabda48b7d5bdbb98f98df811e
Akash Srivastava <vyomsrivastav@gmail.com>
2015-01-22 16:19:59 +0530

    Added file upload functionality

--- app/helpers/articles_helper.rb
+++ app/helpers/articles_helper.rb
@@ -1,6 +1,6 @@
 module ArticlesHelper
   
   def params_article
-    params.require(:article).permit(:title,:body,:tag_list)
+    params.require(:article).permit(:title,:body,:tag_list,:image)
   end
 end
